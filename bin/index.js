639f8f0bd450596ba61c7945ce3c2ae919a609c5
Adrian Li <li.adrianmc@gmail.com>
2016-12-27 01:30:27 -0500

    figure out entry points

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const shell = require('shelljs');
 const webpack = require('webpack');
 const config = require('../config.js');
 const rimrafSync = require('rimraf').sync;
@@ -7,15 +8,23 @@ const rimrafSync = require('rimraf').sync;
 const INPUT_DIR = process.argv[2] || './src';
 const OUTPUT_DIR = process.argv[3] || 'dist';
 
+const entryPoints = [];
+
+shell.ls(`${INPUT_DIR}/*.js`, `${INPUT_DIR}/*.jsx`).forEach(function(file) {
+  entryPoints.push(file);
+});
+
+console.log(entryPoints);
+
 rimrafSync(OUTPUT_DIR);
 
-const compiler = webpack(config(INPUT_DIR, OUTPUT_DIR));
+const compiler = webpack(config(entryPoints, OUTPUT_DIR));
 
 compiler.run(function(err, stats) {
   if (err) {
     console.log('ERROR:', err);
   }
   if (stats.hasErrors()) {
-    console.log(stats.ToJson());
+    console.log(stats.toJson());
   }
 });
--- config.js
+++ config.js
@@ -3,21 +3,29 @@ var path = require('path');
 // resolving for loaders and presets:
 // https://github.com/babel/babel-loader/issues/166#issuecomment-160866946
 
-const config = (src, out) => ({
+const config = (entryPoints, out) => ({
   resolveLoader: { root: path.join(__dirname, 'node_modules') },
   devtool: 'source-map',
-  entry: src,
+  entry: entryPoints,
   output: {
     path: out,
     filename: 'index.js',
     libraryTarget: 'umd',
+  },
+  externals: {
+    react: 'React',
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         loader: require.resolve('babel-loader'),
-        query: { presets: [require.resolve('babel-preset-latest'), 'react'] },
+        query: { presets: [
+          'babel-preset-latest',
+          'babel-preset-stage-3',
+          'babel-preset-react',
+        ].map(require.resolve),
+        },
       },
     ],
   },
