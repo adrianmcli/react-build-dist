6ea84f42cbd78a4860b4b7fcd00b343de8825d3e
Adrian Li <li.adrianmc@gmail.com>
2016-12-29 06:59:20 -0500

    update dependencies and remove dead code

--- bin/utils.js
+++ bin/utils.js
@@ -1,16 +1,21 @@
-const shell = require('shelljs');
 const path = require('path');
 
-const getEntryPoints = (srcPath) => {
-  const results = [];
-  shell.ls(`${srcPath}/*.js`, `${srcPath}/*.jsx`).forEach(function(file) {
-    results.push(file);
-  });
-  return results;
-};
+// const shell = require('shelljs');
+
+// const getEntryPoints = (srcPath) => {
+//   const results = [];
+//   shell.ls(`${srcPath}/*.js`, `${srcPath}/*.jsx`).forEach(function(file) {
+//     results.push(file);
+//   });
+//   return results;
+// };
 
 const getAbsolutePath = dir => path.resolve(process.cwd(), dir);
 
 const stripExtension = x => x.replace(/\.[^/.]+$/, '');
 
-module.exports = { getEntryPoints, getAbsolutePath, stripExtension };
+module.exports = {
+  // getEntryPoints,
+  getAbsolutePath,
+  stripExtension,
+};
